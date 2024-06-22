07b4aea5fa03900b6e11be9486b59a6b784f64f2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:19:58 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    printf("       _.-;;-._\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-;;-._|\n");
+    printf("'-..-'|   ||   |\n");
+    printf("'-..-'|_.-''-._|");
+
+    return 0;
+}
