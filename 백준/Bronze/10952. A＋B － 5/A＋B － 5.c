94f35b9f397fd6550727e4c0554ff09da969706f
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:22:51 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	while (1)
+	{
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		if (a == 0 && b == 0)
+		{
+			break;
+		}
+
+		printf("%d \n", a + b);
+	}
+
+	return 0;
+}
+
