4d89ab409601d0ec1b7bcb41908e63b484681673
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:28:28 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 1300 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main()
+{
+	int n;
+    scanf("%d",&n);
+
+    printf("%d",(int)pow(2,n));
+
+	return 0;
+}
