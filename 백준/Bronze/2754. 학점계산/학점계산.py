8678b980463f912d785d80fdf4da29c0cd022017
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 01:58:26 +0900

    [Bronze III] Title: 학점계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+ch = input()
+
+if ch == "A+":
+    print(4.3)
+elif ch == "A0":
+    print(4.0)
+elif ch == "A-":
+    print(3.7)
+elif ch == "B+":
+    print(3.3)
+elif ch == "B0":
+    print(3.0)
+elif ch == "B-":
+    print(2.7)
+elif ch == "C+":
+    print(2.3)
+elif ch == "C0":
+    print(2.0)
+elif ch == "C-":
+    print(1.7)
+elif ch == "D+":
+    print(1.3)
+elif ch == "D0":
+    print(1.0)
+elif ch == "D-":
+    print(0.7)
+elif ch == "F":
+    print(0.0)
