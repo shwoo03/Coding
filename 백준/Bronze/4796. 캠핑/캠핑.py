351c1527e06bd8947d08b725fa1ce56d99765e9f
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:14:34 +0900

    [Bronze I] Title: 캠핑, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -0,0 +1,10 @@
+count = 1
+
+while True:
+    L,P,V = map(int,input().split())
+    if L == 0 and P == 0 and V == 0:
+        break
+    else:
+        result = (V // P) * L + min((V % P), L)
+        print(f"Case {count}: {result}")
+        count += 1
