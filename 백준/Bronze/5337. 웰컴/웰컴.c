c6aa84a2336f9768046ea33060302978aaba3c86
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:19:53 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+	printf(".  .   .\n");
+	printf("|  | _ | _. _ ._ _  _\n");
+	printf("|/\\|(/.|(_.(_)[ | )(/.\n"); 
+
+	return 0;
+}
