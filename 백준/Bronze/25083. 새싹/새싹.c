b32d2004a85f435488083cf91c60e0a97fa0e454
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-06-23 02:21:30 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	printf("         ,%c\'\"%d\n", 'r', 7); 
+	printf("%c`-_   ,\'  ,/\n", 'r');
+	printf(" \\. \". %c_%c\'\n", 'L', 'r'); 
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
+
