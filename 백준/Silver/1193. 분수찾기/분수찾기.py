18ba8da6e87baffc92a22ae0cc07ba58ca84af9f
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-07-21 19:27:23 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,24 +1,44 @@
-def find_fraction(X):
-    # X번째 대각선이 몇 번째인지 찾기
-    diagonal = 1
-    while X > (diagonal * (diagonal + 1)) // 2:
-        diagonal += 1
-    
-    # X번째 대각선에서의 위치 찾기
-    position_in_diagonal = X - (diagonal * (diagonal - 1)) // 2
-    
-    if diagonal % 2 == 0:
-        numerator = position_in_diagonal
-        denominator = diagonal - position_in_diagonal + 1
-    else:
-        numerator = diagonal - position_in_diagonal + 1
-        denominator = position_in_diagonal
-    
-    return f"{numerator}/{denominator}"
-
-# 예제 입력
 N = int(input())
 
-# 결과 출력
-fraction = find_fraction(N)
-print(fraction)
+diagonal = 1
+while N > (diagonal * (diagonal + 1)) // 2:
+    diagonal += 1
+
+position_in_diagonal = N - (diagonal * (diagonal - 1)) // 2
+
+if diagonal % 2 == 0:
+    numerator = position_in_diagonal
+    denominator = diagonal - position_in_diagonal + 1
+else:
+    numerator = diagonal - position_in_diagonal + 1
+    denominator = position_in_diagonal
+
+print(f"{numerator}/{denominator}")
+
+
+# 시간 초과 ㄷㄷㄷㄷㄷ
+# N = int(input())
+
+# line = 0
+# repeat = 0
+
+# for i in range(N):
+#     repeat += 1
+#     if repeat >= line:
+#         line += 1
+#         repeat = 0
+    
+#     if line % 2 == 0:
+#         a = 1
+#         b = line
+#         for j in range(repeat):
+#             a += 1
+#             b -= 1
+#     elif line % 2 == 1:
+#         a = line
+#         b = 1
+#         for j in range(repeat):
+#             a -= 1
+#             b += 1
+
+# print(f'{a}/{b}')
