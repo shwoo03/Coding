e7fcdea3528ceb8191980c7037509a65e4d120ae
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2024-08-21 09:14:49 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 50540 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,17 @@
+# N 개의 정수가 주어졌을 때, 안에 X라는 정수가 존재하는지 알아내는 프로그램 작성 
+# 존재하면 1, 안하면 0 
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+A = set(map(int, input().split())) 
+
+M = int(input())
+B = list(map(int, input().split()))
+
+for num in B:
+    if num in A:
+        print(1)
+    else:
+        print(0)
