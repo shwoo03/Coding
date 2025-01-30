c607be37e756a09d23ac7a20bfd92be2cfd1ab2d
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-01-30 17:19:21 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+int main() {
+    printf("     /~\\\n");
+    printf("    ( oo|\n");
+    printf("    _\\=/_\n");
+    printf("   /  _  \\\n");
+    printf("  //|/.\\|\\\\\n");
+    printf(" ||  \\ /  ||\n");
+    printf("============\n");
+    printf("|          |\n");
+    printf("|          |\n");
+    printf("|          |\n");
+
+    return 0;
+}
