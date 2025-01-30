035d1ba7d2e6884ee9960c3f64600218278575d1
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-01-30 17:18:28 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+int main(){
+    printf("    8888888888  888    88888\n");
+    printf("   88     88   88 88   88  88\n");
+    printf("    8888  88  88   88  88888\n");
+    printf("       88 88 888888888 88   88\n");
+    printf("88888888  88 88     88 88    888888\n");
+    printf("\n");
+    printf("88  88  88   888    88888    888888\n");
+    printf("88  88  88  88 88   88  88  88\n");
+    printf("88 8888 88 88   88  88888    8888\n");
+    printf(" 888  888 888888888 88  88      88\n");
+    printf("  88  88  88     88 88   88888888\n");
+
+    return 0;
+}
