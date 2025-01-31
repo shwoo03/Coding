58f24677eaa8aaf28ac86681ce811f6412405c4c
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-01-31 18:54:27 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+int main() {
+    long long n;
+
+    scanf("%lld", &n);
+    printf("%lld\n", n*4);
+
+    return 0;
+}
