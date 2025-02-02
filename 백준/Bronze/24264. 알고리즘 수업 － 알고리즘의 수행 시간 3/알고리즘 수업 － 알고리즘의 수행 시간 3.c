ced00edc5168d0ccd0f4ddf8883f9f0f237ffe5f
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-02-02 15:58:44 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    
+    printf("%lld\n", n * n);
+    printf("2\n");
+    
+    return 0;
+}
