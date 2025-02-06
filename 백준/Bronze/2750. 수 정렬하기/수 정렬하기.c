af028d2ae9a000d11a862c1f65195388468ac8f9
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-02-06 11:52:51 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    for(int i = 0; i < N; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            if(arr[i] > arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
