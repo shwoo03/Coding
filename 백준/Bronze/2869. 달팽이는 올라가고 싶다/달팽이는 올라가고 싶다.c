7516824392e40e12b472898e21335ce4d4145f21
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-02-15 20:56:40 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+
+int main() {
+    int rise, fall, goal;
+    scanf("%d %d %d", &rise, &fall, &goal);
+
+    int days = 0;
+
+    // 시간 제한 때문에 그냥 무지성 코드 작성은 안될듯 
+    // days = (goal - rise) / (rise - fall) + 1;
+
+    if (rise >= goal) {
+        printf("1\n");
+    } else {
+        printf("%d\n", (int)ceil((double)(goal - rise) / (rise - fall)) + 1);
+    }
+
+    return 0;
+}
