4bd383f33864a760b7a28e7c2fa542d360dce36e
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-02-18 17:24:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 184 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <limits.h>
+
+#define TRUE 1 
+#define FALSE 0
+
+
+#define ROW 20000
+#define COL 100
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    int num = 666;
+    int count = 0;
+    while (TRUE) {
+        char str[20];
+        sprintf(str, "%d", num);
+        
+        if (strstr(str, "666") != NULL) {
+            count++;
+            if (count == N) {
+                printf("%d\n", num);
+                break;
+            }
+        }
+        
+        num++;
+
+        if(count == N){
+            printf("%d\n", num);
+            break;
+        }
+    }
+
+    return 0;
+}
