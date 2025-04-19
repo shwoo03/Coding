cdcf48ffb9240afc773a34a78f4b781e96b099e2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 23:25:19 +0900

    [Bronze IV] Title: 영수증, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.py
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -1,12 +1,24 @@
-total_price = int(input())
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-check = 0
-for i in range(n):
-    price,num = map(int,input().split())
-    check += price * num
 
-if check == total_price:
-    print("Yes")
-else:
-    print("No")
+
+
+
+
+
+
+if __name__ == "__main__":
+    total = int(input())
+    N = int(input())
+
+    c_num = 0
+    for i in range(N):
+        money, q = map(int,input().split())
+        c_num += (money * q)
+    
+    if(c_num == total):
+        print("Yes")
+    else:
+        print("No")
+
