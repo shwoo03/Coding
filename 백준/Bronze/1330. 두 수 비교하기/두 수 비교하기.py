f0b2f1ac3b938caa801b1f87f5a7dd17c58f624e
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 21:52:02 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,8 +1,19 @@
-a,b = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-if a > b:
-    print(">")
-elif a < b:
-    print("<")
-else:
-    print("==")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    a,b = map(int, input().split())
+
+    if(a > b):
+        print(">")
+    elif(a < b):
+        print("<")
+    else:
+        print("==")
