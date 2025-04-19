30a87df6fd97d5c503fa9ee0b403ca3cfcc2db88
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 21:51:18 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,12 +1,23 @@
-score = int(input())
+import sys
+input = sys.stdin.readline
 
-if score <= 100 and score >= 90:
-    print("A")
-elif score >= 80:
-    print("B")
-elif score >= 70:
-    print("C")
-elif score >= 60:
-    print("D")
-else:
-    print("F")
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    score = int(input())
+
+    if(score > 89):
+        print("A")
+    elif(score > 79):
+        print("B")
+    elif(score > 69):
+        print("C")
+    elif(score > 59):
+        print("D")
+    else:
+        print("F")
