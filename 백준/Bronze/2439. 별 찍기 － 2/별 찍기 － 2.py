9b343f83dfa795d768ba35e19b3734af61ef122a
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 23:12:49 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,4 +1,15 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
 
-for i in range(n):
-    print(" "*(n-(i+1))+"*"*(i+1))
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+
+    for i in range(N):
+        print(" " * (N - i - 1) + "*" * (i + 1))
