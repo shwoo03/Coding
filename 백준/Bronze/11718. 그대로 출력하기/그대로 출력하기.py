fa2c925f8d337f38077880f8ae8424f8096b77a1
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-20 00:05:59 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,6 +1,11 @@
-while True:
-    try:
-        ch = input()
-        print(ch)
-    except:
-        break
+import sys
+input = sys.stdin.readline
+
+
+
+
+
+
+if __name__ == "__main__":
+    for string in sys.stdin:
+        print(string, end='')
