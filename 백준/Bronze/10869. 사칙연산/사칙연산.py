fb58641d273193e560299fa56e7ce6f2bf0c7414
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 21:48:50 +0900

    [Bronze V] Title: 사칙연산, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,7 +1,18 @@
-a,b = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-print(a+b)
-print(a-b)
-print(a*b)
-print(a//b)
-print(a%b)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a//b)
+    print(a%b)
+    
