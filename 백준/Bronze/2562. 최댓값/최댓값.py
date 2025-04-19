815ae76846b8153692003f260a2f5775424cca67
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 23:57:35 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,6 +1,17 @@
-list_num = []
-for i in range(9):
-    list_num.append(int(input()))
+import sys
+input = sys.stdin.readline
 
-print(max(list_num))
-print(list_num.index(max(list_num))+1)
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    num_list = []
+    for i in range(9):
+        num_list.append(int(input()))
+
+    print(max(num_list))
+    print(num_list.index(max(num_list)) + 1)
