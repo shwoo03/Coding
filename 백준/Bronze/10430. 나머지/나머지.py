be641c186a42c2445e2a71cd27e98ea01d32b9c0
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-19 21:54:55 +0900

    [Bronze V] Title: 나머지, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    A,B,C = map(int,input().split())
+
+    print((A+B)%C)
+    print(((A%C) + (B%C))%C)
+    print((A*B)%C)
+    print(((A%C) * (B%C))%C)
