7fe4681d3353c81a519d1eb45427ffd6fcbcccd3
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-20 18:51:52 +0900

    [Bronze II] Title: 나머지, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,7 +1,17 @@
-list_n = []
+import sys
+input = sys.stdin.readline
 
-for i in range(10):
-    n = int(input())
-    list_n.append(n%42)
 
-print(len(list(set(list_n))))
+# 42로 나눈 나머지 이므로 중복 결과를 없애고 len을 구하면 된다.
+
+
+
+if __name__ == "__main__":
+    num_list = []
+
+    for i in range(10):
+        num_list.append(int(input()) % 42)
+    
+    print(len(set(num_list)))
+
+    
