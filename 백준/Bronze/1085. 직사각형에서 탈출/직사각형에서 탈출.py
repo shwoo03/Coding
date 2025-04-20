6325e217d245d0ad992352efad9694d6f3bb8f9a
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-20 15:29:10 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,16 +1,13 @@
-x,y,w,h = map(int,input().split())
+import sys
+input = sys.stdin.readline
 
-if x < w - x:
-    min_width = x
-else:
-    min_width = w - x
 
-if y < h - y:
-    min_length = y
-else:
-    min_length = h - y
 
-if min_length < min_width:
-    print(min_length)
-else:
-    print(min_width)
+
+
+
+if __name__ == "__main__":
+    x,y,w,h = map(int, input().split())
+
+    result = min(w-x, x, y, h-y)
+    print(result)
