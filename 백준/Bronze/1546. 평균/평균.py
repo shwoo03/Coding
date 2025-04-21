300f45f6f582a330ae2f4441689103b8e26a9c18
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-21 15:44:48 +0900

    [Bronze I] Title: 평균, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,8 +1,23 @@
-n = int(input())
-list_score = list(map(int,input().split()))
-m = max(list_score)
+import math
+import sys
+input = sys.stdin.readline
 
-for i in range(n):
-    list_score[i] = list_score[i] / m * 100
 
-print(sum(list_score)/n)
+
+# 수 받고 최대값 찾아서 모든 값에 n/M*100 해주고 평균 구하기
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+
+    list_num = list(map(int, input().split()))
+
+    list_num.sort()
+    max_num = list_num[-1]
+
+    new_score = []
+    for num in list_num:
+        new_score.append(num/max_num*100)
+    
+    print((sum(new_score)) / N)
