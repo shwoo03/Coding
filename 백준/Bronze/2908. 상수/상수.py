6c2a6d66b835abbde947a73e881a957e791e938e
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-21 15:25:38 +0900

    [Bronze II] Title: 상수, Time: 96 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,9 +1,19 @@
-a,b = map(str,input().split())
+import sys
+input = sys.stdin.readline
 
-a = int(a[::-1])
-b = int(b[::-1])
 
-if a > b:
-    print(a)
-elif a < b:
-    print(b)
+
+# 수를 거꾸로 읽어서 수를 비교하는 프로그램 작성 
+
+
+if __name__ == "__main__":
+    x, y = map(str, input().split())
+
+    x = x[::-1]
+    y = y[::-1]
+
+    if(int(x) > int(y)):
+        print(int(x))
+    else:
+        print(int(y))
+
