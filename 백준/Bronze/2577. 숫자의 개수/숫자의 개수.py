f594bf32470f9ed15b7a3f8798bfa8808410a662
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-21 14:58:16 +0900

    [Bronze II] Title: 숫자의 개수, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,21 @@
-a = int(input())
-b = int(input())
-c = int(input())
-mul = a*b*c
+import sys
+input = sys.stdin.readline
 
-list_num = [0 for i in range(10)]
 
-for num in str(mul):
-    num = int(num)
-    list_num[num%10] += 1
 
-for i in list_num:
-    print(i)
+
+
+if __name__ == "__main__":
+    num = 1
+
+    for i in range(3):
+        num *= int(input())
+    
+    num_list = list(str(num))
+    result = [0] * 10
+    for n in num_list:
+        result[int(n)] += 1
+
+    for count in result:
+        print(count)
+
