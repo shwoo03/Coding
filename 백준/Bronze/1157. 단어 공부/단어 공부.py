ff34f5fffc47c22335a8a449b79976f49bb142c2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-23 21:37:38 +0900

    [Bronze I] Title: 단어 공부, Time: 112 ms, Memory: 114496 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,15 +1,31 @@
-ch = input().lower()
+import math
+import sys
+input = sys.stdin.readline
 
-list_count = [0 for i in range(26)]
 
-for c in ch:
-    if 'a' <= c <= 'z':  # 알파벳인 경우에만 처리
-        list_count[ord(c) - ord('a')] += 1
 
-max_count = max(list_count)
+# 대소문자 알파벳이 주어지면 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램 작성 
 
-if list_count.count(max_count) > 1:
-    print("?")
-else:
-    max_index = list_count.index(max_count)
-    print(chr(max_index + ord('a')).upper())
+# 1. 모두 대문자로 바꾸기 
+# 2. 배열로 알파벳 개수 리스트 만들기 
+# 3. for로 모두 돌고 max() 함수로 출력 
+
+
+if __name__ == "__main__":
+    string = input().strip()
+    string = string.lower()
+    list_alpha = [0] * 26
+
+    for ch in string:
+        index = ord(ch) - ord('a')
+        list_alpha[index] += 1
+    
+    if list_alpha.count(max(list_alpha)) != 1:
+        print("?")
+    else:
+        max_alpha_index = list_alpha.index(max(list_alpha))
+        print(chr(max_alpha_index + ord('A')))
+
+
+
+
