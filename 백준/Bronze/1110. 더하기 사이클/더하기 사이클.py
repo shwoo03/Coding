49ffed1c7277a0de23170c36102cea8943c461e3
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-23 21:15:44 +0900

    [Bronze I] Title: 더하기 사이클, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,15 +1,36 @@
-org_num = int(input())
-n = org_num
-cnt = 0
+import math
+import sys
+input = sys.stdin.readline
 
-while True:
-    a = n // 10
-    b = n % 10
-    c = (a + b) % 10
-    n = (b * 10) + c
 
-    cnt += 1
-    if(n == org_num):
-        break
 
-print(cnt)
+# 0 <= x <= 99 일때 10 보다 작으면 0 붙이고 각 자리 수를 더함 
+# 주어진 수의 오른쪽과 계산 값의 오른쪽 수를 이어 붙여서 새로운 수 만들기 
+# 26 -> 2 + 6 = 8
+# 68 -> 6 + 8 = 14
+# 84 
+
+
+
+if __name__ == "__main__":
+    num = input().strip()
+    
+    # 1자리 수일 경우 0붙이기
+    if len(num) == 1:
+        num = '0' + num
+    
+    original_num = num
+    count = 0
+
+    while True:
+        # 각 자리수 더하기
+        sum_digits = int(num[0]) + int(num[1])
+        # 새로운 숫자 만들기
+        num = num[1] + str(sum_digits % 10)
+        count += 1
+        
+        if num == original_num:
+            break
+
+    print(count)
+
