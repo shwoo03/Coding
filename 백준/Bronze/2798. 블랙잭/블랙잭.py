067cbc248866ba7fc4b204e5a6e81bd5d0ef0cc2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-23 20:58:26 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,19 +1,27 @@
-#최대 합은 21 
-# 각 카드는 양의 정수 
-# n 장의 카드를 모두 숫자가 보이도록 바닥에 놓고 숫자 M을 크게 외침
-# 플레이어는 시간안에 n장의 카드 중 3장을 골라야 함 
-# 단 합은 M을 넘지 않고 최대한 M에 가깝게 만든다. 
+import math
+import sys
+input = sys.stdin.readline
 
-n,m = map(int,input().split())
-list_num = list(map(int,input().split()))
-result = 0
 
-for first in range(0,n-2,1):
-    temp = 0
-    for second in range(first+1,n-1,1):
-        for third in range(second+1,n,1):
-            temp = list_num[first] + list_num[second] + list_num[third]
-            if m - result > m - temp and temp <= m:
-                result = temp
 
-print(result)
+# 주어진 M을 넘지 않는 한도에서 카드 합을 최대로 만들어야 한다. 
+
+
+
+if __name__ == "__main__":
+    card_num, max_num = map(int,input().split())
+
+    list_num = list(map(int, input().split()))
+    max = 0
+    
+    for first_num in range(card_num-2):
+        for second_num in range(first_num+1, card_num-1):
+            for third_num in range(second_num+1, card_num):
+                sum = list_num[first_num] + list_num[second_num] + list_num[third_num]
+
+                if max < sum and sum <= max_num:
+                    max = sum
+    
+    print(max)
+
+
