36dcd9c1d6df7968422ff8bf58172169f60abcc9
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-24 11:17:17 +0900

    [Bronze I] Title: 소인수분해, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,16 +1,36 @@
-n = int(input())
+import math
+import sys
+input = sys.stdin.readline
 
-i = 2
-factors = []
-while i * i <= n:
-    if n % i:
+# 소인수 분해 프로그램 
+
+def get_result(num):
+    list_num = []
+    i = 2
+
+    while i * i <= num:
+        while num % i == 0:
+            list_num.append(i)
+            num //= i
+        
         i += 1
-    else:
-        n //= i
-        factors.append(i)
 
-if n > 1:
-    factors.append(n)
+    if num > 1:
+        list_num.append(num)
+    
+    return list_num
 
-for num in factors:
-    print(num)
+
+        
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+
+    list_num = get_result(N)
+    for num in list_num:
+        print(num)
+
+
+
