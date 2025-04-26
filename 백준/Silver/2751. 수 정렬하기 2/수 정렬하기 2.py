ddb64e8f58660794859e93af483b2d59e70edafc
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-26 16:52:11 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 564 ms, Memory: 188968 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,15 +1,23 @@
+import math
 import sys
-input = sys.stdin.read
-output = sys.stdout.write
+input = sys.stdin.readline
 
-def main():
-    data = input().split()
-    N = int(data[0])
-    list_num = list(map(int, data[1:]))
-    
-    list_num.sort()
-    
-    output('\n'.join(map(str, list_num)) + '\n')
+
 
 if __name__ == "__main__":
-    main()
+    T = int(input())
+    list_num = []
+
+    for i in range(T):
+        list_num.append(int(input()))
+    
+    list_num.sort()
+    for n in list_num:
+        print(n)
+
+
+
+
+
+
+
