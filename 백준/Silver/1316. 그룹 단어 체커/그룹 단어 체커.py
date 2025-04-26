0c1e44b49742823c75b9e93a16ba4485a81c4d24
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-26 16:50:07 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,27 +1,41 @@
-# 그룹 단어란 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다.
+import math
+import sys
+input = sys.stdin.readline
 
-N = int(input())   
-result = 0
 
-for i in range(N):
-    word = input()
-    is_group = True
-    # 중복 문자 체크 변수 
-    duplicate_check = []
-    
-    # 이전 문자 기록 변수
-    prev_char = ''
-    for ch in word:
-        if ch != prev_char:
-            if ch in duplicate_check:
-                is_group = False
-                break
-            prev_char = ch
-            duplicate_check.append(ch)
-        elif ch == prev_char:
-            continue
-    
-    if is_group:
-        result += 1
+# 각 단어는 연속하지 않는 이상 한 번만 나와야 한다.
+# 이런것을 그룹단어라고 하는데 그룹 단어의 수를 게산해야 함 
+# 일단 중복
 
-print(result)
+if __name__ == "__main__":
+    T = int(input())
+    count = 0
+
+    for i in range(T):
+        word = input().strip()
+        is_group = True
+        word_list = []
+        prev_ch = ''
+
+        for ch in word:
+            if ch != prev_ch:
+                if ch in word_list:
+                    is_group = False
+                    break
+
+                word_list.append(ch)
+            elif ch == prev_ch:
+                continue
+            
+            prev_ch = ch
+        
+        if is_group:
+            count += 1
+        
+    print(count)
+
+
+
+
+
+
