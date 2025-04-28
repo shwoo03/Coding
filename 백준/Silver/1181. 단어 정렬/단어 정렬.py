635b5de043dcbd9de3f3152d9ef43ea92ab26525
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-28 18:06:21 +0900

    [Silver V] Title: 단어 정렬, Time: 164 ms, Memory: 115900 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,11 +1,25 @@
-N = int(input())
+import math
+import sys
+input = sys.stdin.readline
 
-list_chr = []
-for i in range(N):
-    list_chr.append(input())
 
-list_chr = list(set(list_chr))  # 중복 제거
-list_chr.sort(key=lambda x: (len(x), x))  # 길이 우선, 길이가 같으면 사전순으로 정렬
+# 알파벳 소문자로 이루어진 N개의 단어가 들어오면 
+# 1. 길이가 짧은 것부터 
+# 2. 길이가 같으면 사전 순으로 정렬 
+# 3. 중복 단어는 하나만 남기고 제거 
 
-for ch in list_chr:
-    print(ch)
+
+
+if __name__ == "__main__":
+    T = int(input())
+    list_words = []
+
+    for i in range(T):
+        list_words.append(input().strip())
+
+    
+    list_words = list(set(list_words))
+    list_words = sorted(list_words, key=lambda x: (len(x), x))
+    
+    for ch in list_words:
+        print(ch)
