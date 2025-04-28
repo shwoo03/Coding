e9e1c7421834f615bb5a6a902fe3c06bf54f9f25
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-28 17:53:33 +0900

    [Silver V] Title: 셀프 넘버, Time: 104 ms, Memory: 111088 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,23 +1,26 @@
-# 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수 
-# 예를 들어, d(75) = 75+7+5 = 87이다.
+import math
+import sys
+input = sys.stdin.readline
 
-# 이것으로 무한 수열을 만들 수 있다. 
-# 그렇다면 생성자 함수에서 10000 까지의 수를 빼면 된다. 
 
-def d(n):
-    result = n
-    while n > 0:
-        result += n % 10
-        n = n // 10
-    return result
+# 셀프 넘버는 n과 n의 각 자리 수를 더하는 함수다 
+# 이때 33은 39의 생성자이고 39는 51의 생성자이다. 
+# 하지만 생성자가 없는 숫자를 셀프 넘버라고 하는데 셀프 넘버를 모두 출력하는 코드 작성 
+# 10000 보다 작거나 같은 수 기준 
 
-numbers = set(range(1, 10001))
-generated_numbers = set()
 
-for i in range(1, 10001):
-    generated_numbers.add(d(i))
+# 생성자 수를 모두 구하고 중복을 없앤다음 
+# 10000 이하의 값에서 list에 없는 값 출력 
 
-self_numbers = numbers - generated_numbers
 
-for self_number in sorted(self_numbers):
-    print(self_number)
+if __name__ == "__main__":
+    list_self_num = set()
+
+    for i in range(1, 10001):
+        list_self_num.add(i + sum(map(int, str(i))))
+    
+    for i in range(1, 10001):
+        if i not in list_self_num:
+            print(i)
+
+
