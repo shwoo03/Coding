5cf593a71bf6f0f771f8d0561f2c1720c1407f79
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-30 08:59:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,18 +1,28 @@
-# 상근이는 설탕 N KG 배달해야함 
-# 설탕은 3KG 또는 5KG 봉지에 담겨져 있음
-# 최대한 적은 봉지를 들고 가려고 함
-# N KG를 배달하기 위해 가져가야 하는 봉지의 최소 개수를 출력
-# 만들 수 없다면 -1 출력 
+import math
+import sys
+input = sys.stdin.readline
 
-N = int(input())
 
-result = 0
-while True:
-    if N % 5 == 0:
-        result += N // 5
-        break
+# 상근이가 N킬로그램을 배달해야 한다. 
+# 3, 5 킬로그램 봉지가 있는데 가장 작은 개수의 봉투를 가지고 가려고 함 
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+    dp = [10000] * (N+1)
+    dp[0] = 0
+
+    # 3KG -> 5KG 봉투를 추가할 수 있는지의 순서로 작성해야 함 
+    for i in range(1, N+1):
+        if i >= 3:
+            dp[i] = min(dp[i], dp[i-3] + 1)
+
+        if i >= 5:
+            dp[i] = min(dp[i], dp[i-5] + 1)
     
-    N -= 3
-    result += 1
+    if dp[N] == 10000:
+        print(-1)
+    else:
+        print(dp[N])
 
-print(-1) if N < 0 else print(result)
