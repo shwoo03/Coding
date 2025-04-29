c70b65b538b0596e07efdc251bf88e427d2f6840
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-30 00:12:48 +0900

    [Silver V] Title: 소트인사이드, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,7 +1,15 @@
-input_str = input()
-numbers = list(map(int, list(input_str)))
+import math
+import sys
+input = sys.stdin.readline
 
-numbers.sort(reverse=True)
 
-for number in numbers:
-    print(number, end='')
+# 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정리하기 
+
+
+if __name__ == "__main__":
+    num = input().strip()
+    num_list = list(map(int, num))
+    num_list.sort(reverse=True)
+
+    for n in num_list:
+        print(n,end='')
