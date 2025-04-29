a0d1be0ec4d60f36f786baf9f826b0db2012d401
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-30 00:26:53 +0900

    [Silver V] Title: 분수찾기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,44 +1,25 @@
-N = int(input())
-
-diagonal = 1
-while N > (diagonal * (diagonal + 1)) // 2:
-    diagonal += 1
-
-position_in_diagonal = N - (diagonal * (diagonal - 1)) // 2
-
-if diagonal % 2 == 0:
-    numerator = position_in_diagonal
-    denominator = diagonal - position_in_diagonal + 1
-else:
-    numerator = diagonal - position_in_diagonal + 1
-    denominator = position_in_diagonal
-
-print(f"{numerator}/{denominator}")
+import math
+import sys
+input = sys.stdin.readline
 
 
-# 시간 초과 ㄷㄷㄷㄷㄷ
-# N = int(input())
 
-# line = 0
-# repeat = 0
 
-# for i in range(N):
-#     repeat += 1
-#     if repeat >= line:
-#         line += 1
-#         repeat = 0
-    
-#     if line % 2 == 0:
-#         a = 1
-#         b = line
-#         for j in range(repeat):
-#             a += 1
-#             b -= 1
-#     elif line % 2 == 1:
-#         a = line
-#         b = 1
-#         for j in range(repeat):
-#             a -= 1
-#             b += 1
+if __name__ == "__main__":
+    X = int(input())
 
-# print(f'{a}/{b}')
+    line = 1
+    while X > (line * (line + 1)) // 2:
+        line += 1
+
+    start_num = (line * (line - 1)) // 2 + 1
+    pos_in_line = X - start_num + 1
+
+    if line % 2 == 0:
+        numerator = pos_in_line
+        denominator = line - pos_in_line + 1
+    else:
+        numerator = line - pos_in_line + 1
+        denominator = pos_in_line
+
+    print(f"{numerator}/{denominator}")
