9815faf7c6ddf7549a01c1cf59b972424483c0f7
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-04-30 00:23:09 +0900

    [Silver V] Title: 영화감독 숌, Time: 168 ms, Memory: 110576 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,12 +1,25 @@
-N = int(input())
+import math
+import sys
+input = sys.stdin.readline
 
-count = 0
-num = 666
 
-while True:
-    if '666' in str(num):
-        count += 1
-    if count == N:
-        print(num)
-        break
-    num += 1
+# 666 들어간 종말 수 찾기 
+
+
+if __name__ == "__main__":
+    N = int(input())
+    if N == 1:
+        print("666")
+    elif N == 2:
+        print("1666")
+    else:
+        count = 2
+        num = 2665
+
+        while True:
+            num += 1
+            if "666" in str(num):
+                count += 1
+                if count == N:
+                    print(num)
+                    break
