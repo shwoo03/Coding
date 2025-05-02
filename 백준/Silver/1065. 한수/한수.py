5171fa364d35b4abf57e19a4b9f276dffa4160d4
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-02 15:54:09 +0900

    [Silver IV] Title: 한수, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,18 +1,27 @@
-# 정수 X의 각 자리가 등차수열을 이루면, 그 수를 한수라고 한다. 
-# 등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다.
-# N이 주어지고, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오.
+import math
+import sys
+input = sys.stdin.readline
 
-N = int(input())
 
-def hansu(N):
+# 어떤 양의 정수 x의 각 자리가 등차수열을 이룬다면 그 수를 한수라고 함 
+# 한수의 개수를 출력하는 프로그램 작성 N이하의 수 
+
+def is_hansu(x):
+    num = list(map(int, str(x)))
+    if len(num) <= 2:
+        return True
+    else:
+        return (num[0] - num[1]) == (num[1] - num[2])
+
+
+if __name__ == "__main__":
+    N = int(input())
+
+    # 1, 10, 100의 자리일 때로 경우를 나눠서 판단하면 됨 
     count = 0
-    for i in range(1, N+1):
-        if i < 100:
+    for i in range(1, N + 1):
+        if is_hansu(i):
             count += 1
-        else:
-            num = list(map(int, str(i)))
-            if num[0] - num[1] == num[1] - num[2]:
-                count += 1
-    return count
 
-print(hansu(N))
+    print(count)
+    
