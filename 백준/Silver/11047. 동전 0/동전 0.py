bf1a92ed850b525fa86bad4e53a0f8f7bcac18b4
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-02 15:59:53 +0900

    [Silver IV] Title: 동전 0, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,20 +1,30 @@
-# 준규가 가지는 동전은 N개의 종류이다.
-# 각각의 동전은 매우 많이 가지고 있다.
-# 그 가치의 합을 K로 만들려고 한다.
+import math
+import sys
+input = sys.stdin.readline
 
-# 이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
 
-N, K = map(int, input().split())
+# 준규가 가진 동전 종류는 N개
+# 동전을 적절히 사용해서 그 가치의 합을 K로 만드려고 한다.
+# 동전 개수의 최솟값을 구하는 프로그램 작성 
 
-coin = []
-for i in range(N):
-    coin.append(int(input()))
 
-coin.sort(reverse=True)
-result = 0
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    count = 0
 
-for i in coin:
-    result += K // i
-    K %= i
+    N_list = []
+    for i in range(N):
+        N_list.append(int(input()))
+    N_list.sort(reverse=True)
 
-print(result)
+    i = 0
+    while True:
+        count += K // N_list[i]
+        K %= N_list[i]
+
+        if K <= 0:
+            print(count)
+            break
+        else:
+            i += 1
+
