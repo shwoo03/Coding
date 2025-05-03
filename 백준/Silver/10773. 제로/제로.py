a43b5644ee3a6d9a69da86a0febe8d8c5e5ec94b
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-03 15:07:30 +0900

    [Silver IV] Title: 제로, Time: 104 ms, Memory: 114580 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,15 +1,30 @@
-# 재민이는 돈을 관리하고 있다. 
-# 근데 실수가 많아서 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 된다. 
-# 모든 수를 받아 적은 후 그 수의 합을 알고 싶어 한다. 
+import math
+import sys
+input = sys.stdin.readline
 
-K = int(input())
-stack = []
 
-for _ in range(K):
-    num = int(input())
-    if num == 0:
-        stack.pop()
-    else:
-        stack.append(num)
 
-print(sum(stack))
+# 0이 들어오면 하나씩 빼면 됨 
+
+
+if __name__ == "__main__":
+    K = int(input())
+    list_num = []
+
+    for i in range(K):
+        n = int(input())
+
+        if n == 0:
+            if list_num:
+                list_num.pop()
+            else:
+                continue
+        else:
+            list_num.append(n)
+
+    sum = 0
+    for num in list_num:
+        sum += num
+    
+    print(sum)
+
