fa4ca67a4fc06b027448b92f7d948e12f6c198e1
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-03 15:07:23 +0900

    [Silver IV] Title: 수 찾기, Time: 176 ms, Memory: 144232 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,22 +1,23 @@
-# N 개의 정수가 주어졌을 때, 안에 X라는 정수가 존재하는지 알아내는 프로그램 작성 
-# 존재하면 1, 안하면 0 
-
+import math
 import sys
-
 input = sys.stdin.readline
-N = int(input())
-A = set(map(int, input().split())) 
-
-M = int(input())
-B = list(map(int, input().split()))
-
-for num in B:
-    if num in A:
-        print(1)
-    else:
-        print(0)
 
 
-# 정보!
-# list는 모든 요소를 순차탐색 하는데 
-# set은 해시테이블을 사용하기 때문에 O(1)에 탐색 가능하다.
+# 주어진 수 안에서 어떤 숫자가 있는지 확인해야 함 
+# 해싱을 사용하는 자료형인 set 사용하면 될듯 
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+    nums = set(map(int, input().split()))
+
+    check_n = int(input())
+    check_nums = list(map(int, input().split()))
+
+    for num in check_nums:
+        if num in nums:
+            print(1)
+        else:
+            print(0)
+
