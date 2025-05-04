2530e2364db2dd9074897bf8564ab25763a8a923
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-04 13:20:33 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 117320 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,16 +1,20 @@
-# N 장의 카드가 있다.
-# 각각의 카드는 차례로 1부터 N까지의 번호가 있음 
-# 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
-# 이제 제일 위에 있는 카드를 바닥에 버린다.
-# 그 다음 제일 위에 있는 카드를 제일 아래에 잇는 카드 밑으로  옮긴다.
+import math
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-N = int(input())
-list_num = list(range(1, N + 1))  
 
-index = 0 
-while len(list_num) - index > 1: 
-    index += 1  
-    list_num.append(list_num[index]) 
-    index += 1  
+# 1번 카드가 제일 위에 있고 카드 한 장이 남을 때 까지 
+# 1. 제일 위의 카드릴 바닥에 버린다. 
+# 2. 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다. 
 
-print(list_num[index])  
+
+if __name__ == "__main__":
+    N = int(input())
+    card_queue = deque(range(1, N + 1))
+
+    while len(card_queue) > 1:
+        card_queue.popleft()         
+        card_queue.append(card_queue.popleft())  
+
+    print(card_queue[0])
