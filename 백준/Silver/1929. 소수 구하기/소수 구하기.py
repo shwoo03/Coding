e7237454634b7a376023bfb2b06334ae76c1b8b0
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-05 15:13:04 +0900

    [Silver III] Title: 소수 구하기, Time: 784 ms, Memory: 112312 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,17 +1,25 @@
-# M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+import math
+import sys
+input = sys.stdin.readline
 
-def is_prime(num):
-    if num == 1:
+
+
+# M 이상 N 이하의 소수를 모두 출력하는 프로그램 작성 
+
+def is_prime(x):
+    if x < 2:
         return False
-    if num == 2:
-        return True
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+    
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
             return False
+    
     return True
 
-M, N = map(int, input().split())
 
-for i in range(M, N+1):
-    if is_prime(i):
-        print(i)
+if __name__ == "__main__":
+    M, N = map(int, input().split())
+
+    for num in range(M, N+1):
+        if is_prime(num):
+            print(num)
