5de6249f0a385d5766ae3e9e8821cd12055affad
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-05 15:12:24 +0900

    [Silver III] Title: 1로 만들기, Time: 132 ms, Memory: 119828 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,24 +1,34 @@
-# 정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+import math
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-# 1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
-# 2. X가 2로 나누어 떨어지면, 2로 나눈다.
-# 3. 1을 뺀다.
 
-# 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 
-# 연산을 사용하는 횟수의 최솟값을 출력하시오.
+# 정수에 사용할 수 있는 연산은 다음 3가지임 
+# 1. x가 3으로 나누어 떨어지면 3으로 나눔 
+# 2. x가 2로 나누어 떨어지면, 2로 나눔 
+# 3. 1을 뺌 
+# 위 세 개의 연산을 사용해서 1을 만드는 최소 횟수 구하기 
 
-# dp[i-1] + 1 , dp[i//2] +1, dp[i//3] +1 중 최소값을 찾아서 dp[i]에 저장한다.
 
-N = int(input())
-dp = [0] * (N+1)
 
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
+if __name__ == "__main__":
+    N = int(input())
+    dp = [0] * (N+1)
 
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+    for i in range(2, N + 1):
+        # 1을 빼는것은 항상 적용할 수있는 연산이다 
+        # 그렇다면 일단 1을 배고 나누어 떨어지는지 확인해서 
+        # min(dp[i], dp[i // 2 or 3] + 1)
+        # 이런식으로 검사하면 N까지의 모든 dp 리스트가 완성됨 
 
-print(dp[N])
+        dp[i] = dp[i - 1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i // 2] + 1)
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    print(dp[N])
+
