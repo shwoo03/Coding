83684e15e3257fcb2ee9e57bbfe83945fd33a74e
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-06 15:49:36 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,29 @@
-# 정수가 주어지면 1,2,3의 합으로 구성할 수 있는 경우의 수를 구하는 프로그램 
-# 마지막에 1, 2, 3인지 확인하여 dp[n]을 리턴하는 방식으로 해결 
+import math
+import sys
+input = sys.stdin.readline
 
-n = int(input())
-dp = [0] * 11
-dp[0] = 1
 
-for i in range(1, 11):
-    dp[i] = dp[i - 1]
 
-    if i >= 2:
-        dp[i] += dp[i - 2]
-    if i >= 3:
-        dp[i] += dp[i - 3]
+# 정수가 입력되면 n을 1,2,3의 합으로 나타내는 방법의 수를 출력하기 
+# 그렇다면 n-3 까지의 경우의 수 + 1,2,3 의 경우의 수를 모두 구하면 되므로 
+# 1 -> 1
+# 2 -> 11, 2
+# 3 -> 111, 112, 121, 211
+# 4 -> 13, 31, 112, 121, ..........
+# dp[i] = dp[i-1] + dp[i-2] + dp[i-3] 이다.
 
-for _ in range(n):
-    num = int(input())
-    print(dp[num])
+
+if __name__ == "__main__":
+    T = int(input())
+
+    dp = [0] * 10000
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, 12):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    
+    for i in range(T):
+        print(dp[int(input())])
+
