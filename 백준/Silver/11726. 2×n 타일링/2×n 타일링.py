00db9550e958f9c6f0bd80bbc31f64c01ed72746
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-07 10:14:22 +0900

    [Silver III] Title: 2×n 타일링, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,23 +1,26 @@
-# 2xn 크기의 직사각형을 1x2, 2x1 타일로 채우는 경우의 수를 구하는 문제
+import math
+import sys
+input = sys.stdin.readline
 
-# 접근 
-# n이 1이면 1, 
-# n이 2면 2,
-# n이 3이면 3,
-# n이 4면 5,
-# n이 5면 8,
-# 점화식은 dp[i] = dp[i - 1] + dp[i - 2]로 나타낼 수 있다.
 
-n = int(input())
 
-if n == 1:
-    print(1)
-elif n == 2:
-    print(2)
-else:
-    dp = [0] * (n + 1)
+# 2 x n 직사각형을 1x2, 2x1 타일로 채우는 방법의 수를 구하는 프로그램 작성하기 
+# 2 x n 직사각형을 채우는 수의 리스트를 dp[i] 라고 해보면 
+# dp[1] = 1 이다.
+# dp[2] = 2 이다.
+# dp[3] = 3 -> dp[1] + dp[2]
+# dp[4] = 5 -> dp[2] + dp[3]
+# dp[i] = dp[i-1] + dp[i-2] 이다. 결국 2x1 또는 1x2 이므로
+# 이미 완성된 퍼즐에서 붙일 수 있는 수가  i-1, i-2에서의 타일 밖에 없으므로 점화식은 위와 같음 
+
+
+if __name__ == "__main__":
+    n = int(input())
+    dp = [0] * 1001
     dp[1] = 1
     dp[2] = 2
-    for i in range(3, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2]
-    print(dp[n] % 10007)
+
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 10007
+    
+    print(dp[n])
