2d80a1fc98f9e9c23238ca4b66a832766317fa21
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-10 19:58:38 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 110068 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,34 +1,35 @@
-# 계단은 한 번에 한 계단씩 또는 두 계산씩 오를 수 있다.
-# 단 연속 3개를 모두 밟으면 안된다.
-# 마지막 계단은 무조건 밟아야 한다.
-# 점수는 계단의 점수 합계이다.
-# 최대 점수를 구해라 
+import math
+from collections import deque
+import sys
+input = sys.stdin.readline
 
-# 해당 문제는 DP 문제이다.
-# 즉 각 계단에 도달하기 전에 최대 점수를 수해놓고 더하면 된다.
-# 점화식은 다음과 같음 
-# dp[i] = max[dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i]]
 
-# dp[0] = score[0]
-# dp[1] = score[0] + score[1]
-# dp[2] = max[score[0] + score[2], score[1] + score[2]]
-# dp[3] = max[dp[1] + score[3], dp[0] + score[2] + score[3]]
-# dp[4] = max[dp[2] + score[4], dp[1] + score[3] + score[4]]
-# ... 이런식으로 증가하면서 최대 값을 계산하면 됨 
+# 1. 한 번에 한 계단 또는 두 계단씩 오를 수 있음 
+# 2. 한 계단을 밟으면서 이어서 다음 계단이나 다음다음 계단으로 오를 수 있음 
+# 3. 마지막 계단은 무조건 밟아야 함 
+# 얻을 수 있는 점수의 최대값을 구하는 프로그램을 작성
+# 두 칸씩 올라가는 식은 dp[i-2] + list_score[i] 이런식으로 가능하다 
+# 한칸 씩 올라가지만 연속 세 번은 못하는 것은 다음과 같이 표현이 가능할 것이다 
+#     dp[i-3] + list_score[i-1] + list_score[i] -> i-3 까지는 오고 i-2를 건너뛰고 2개를 밟으면 된다 
+# max(dp[i-2] + list_score[i], dp[i-3] + list_score[i-1] + list_score[i])
 
-n = int(input())
-score = [0] * n
 
-for i in range(n):
-    score[i] = int(input())
+if __name__ == "__main__":
+    N = int(input())
+    list_score = [int(input()) for _ in range(N)]
 
-    if i == 0:
-        dp = [score[0]]
-    elif i == 1:
-        dp.append(score[0] + score[1])
-    elif i == 2:
-        dp.append(max(score[0] + score[2], score[1] + score[2]))
+    dp = [0] * (N)
+
+    if N == 1:
+        print(list_score[0])
+    elif N == 2:
+        print(list_score[0] + list_score[1])
     else:
-        dp.append(max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i]))
+        dp[0] = list_score[0]
+        dp[1] = list_score[0] + list_score[1]
+        dp[2] = max(list_score[0] + list_score[2], list_score[1] + list_score[2])
 
-print(dp[n-1])
+        for i in range(3, N):
+            dp[i] = max(dp[i-2] + list_score[i], dp[i-3] + list_score[i-1] + list_score[i])
+        
+        print(dp[N-1])
