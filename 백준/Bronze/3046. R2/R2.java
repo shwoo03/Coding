d0815658461bd4b500d41a941bcfa99649437ce0
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-14 09:23:53 +0900

    [Bronze IV] Title: R2, Time: 176 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+import java.io.*;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int R1 = sc.nextInt();
+        int S = sc.nextInt();
+        out.println(2*S - R1);
+
+
+
+    }
+}
