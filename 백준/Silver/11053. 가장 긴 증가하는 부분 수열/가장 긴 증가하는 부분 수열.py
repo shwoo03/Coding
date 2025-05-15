15e17b44693f7a08b71ab277878d620a4f2be705
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-15 11:21:34 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 238068 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,29 +1,29 @@
+import math
+from collections import deque
+from itertools import permutations, combinations
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 
-# 수열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열의 길이를 구하는 문제
+# 수열 A가 주어지면 가장 긴 증가하는 부분 수열을 구하는 문제
+# [1,1,1,1,1,1,1,1,1,1....] 일 때, 증가하는 부분들에서
+# 모든 경우의 수를 고려해야 하기 때문에 1,2,3 번째 요소들의 
+# 최대 수열의 길이를 고려해야 함 따라서 모든 요소를 순회해야 함 
+#   증가하는 부분 수열이기 때문에 수가 증가해야 하므로 
+#   2중 for문을 사용하여 max 값을 정해놓고 0번 index부터 증가하면서 비교하면 됨  
+# 만약, if list_num[j] < list_num[i] 가 되면 자신보다 작은 수가 존재하므로 해당 index에서 
+#   dp[i] = max[dp[i], dp[j] + 1] 이렇게 하면 됨 
 
 if __name__ == "__main__":
-    n = int(input())
-    a = list(map(int, input().split()))
+    T = int(input())
+    list_num = list(map(int, input().split()))
 
-    # n 번째 수열의 길이를 dp[n]이라고 하면 
-    # dp[n] = max(dp[i]) + 1 (i < n and a[i] < a[n])
-    # dp[0] = 1 (첫 번째 수열은 항상 1)
-    # dp[1] = 2 (첫 번째 수열과 두 번째 수열이 증가하는 경우)
-    # dp[2] = 3 (첫 번째 수열과 두 번째 수열과 세 번째 수열이 증가하는 경우)
-    # dp[3] = 4 (첫 번째 수열과 두 번째 수열과 세 번째 수열과 네 번째 수열이 증가하는 경우)
+    dp = [1] * T
 
-    dp = [1] * n
-
-    for i in range(1,n):
+    for i in range(T):
         for j in range(i):
-            if a[i] > a[j]:
+            if list_num[j] < list_num[i]:
                 dp[i] = max(dp[i], dp[j] + 1)
     
     print(max(dp))
-
-
-
-
