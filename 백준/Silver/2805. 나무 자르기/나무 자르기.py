1c1a1e560a6cf889fe998693ac06a0116488d68d
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-17 15:12:18 +0900

    [Silver II] Title: 나무 자르기, Time: 472 ms, Memory: 262004 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,46 +1,53 @@
+import math
+from collections import deque
+from itertools import permutations, combinations
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-# 나무 M미터가 필요함 목재는 다음과 같이 절단함 
-# 절단기에 H높이를 지정, 한 줄에 연속해있는 나무를 모두 절단 
-# H보다 낮은 나무는 절단되지 않음
-# 즉, 20,15,10,17이면 H=15일때 20-15+15-10+17-15=7
-# 필요한 만큼만 가져갈거임, 최대 높이를 구하는 문제
+# M미터의 나무가 필요함 
+# H미터 지정하여 나무를 자르면 잘린 나무를 가져감 
+# 단, 나무를 필요한 만큼만 가져가려고 함 
+# 절단기에 설정할 수 있는 높이의 최대값을 구하시오 
+# 최대/최소 나무 길이를 가진 요소의 중간부터 탐색을 진행하면 됨 
+def binary_search(list_tree, start, end, target, tree_num):
+    max_tree_high = 0
 
+    while start <= end:
+        mid = (start + end) // 2
+        tree_sum = 0
+        for tree in list_tree:
+            if (tree - mid) <= 0:
+                tree_sum += 0
+            else:
+                tree_sum += tree - mid
+        
 
-# 탐색 해야함 
-def binary_search(trees, target_height, tree_num):
-    trees.sort(reverse=True) # 나무를 내림차순으로 정렬
-    left = 0
-    right = max(trees) # 나무의 최대 높이
-    answer = 0
-
-    while left <= right:
-        mid = (left + right) // 2 # 중간값
-        total = 0 # 나무의 총 길이
-
-        for tree in trees:
-            if tree > mid: # 나무가 mid보다 크면 잘림
-                total += tree - mid # 잘린 나무의 길이 더함
-
-        if total >= target_height: # 잘린 나무의 길이가 목표 높이 이상이면
-            answer = mid # 현재 높이를 정답으로 저장
-            left = mid + 1 # 높이를 올림
+        if tree_sum == target:
+            return mid
+        elif tree_sum < target:
+            end = mid - 1
         else:
-            right = mid - 1 # 높이를 내림
+            start = mid + 1
+            if max_tree_high < mid:
+                max_tree_high = mid
+    
+    return max_tree_high
 
-    return answer
 
 
 
 if __name__ == "__main__":
-    tree_num, target_height = map(int, input().split()) # 나무의 개수, 필요한 나무의 길이
-    trees = list(map(int, input().split())) 
+    tree_num, tree_length = map(int,input().split())
+    list_tree = list(map(int, input().split()))
+    list_tree.sort()
+    result = -1
     
+    print(binary_search(list_tree, 0, list_tree[-1], tree_length, tree_num))
 
-    print(binary_search(trees, target_height, tree_num))
+
+
+
 
 
 
