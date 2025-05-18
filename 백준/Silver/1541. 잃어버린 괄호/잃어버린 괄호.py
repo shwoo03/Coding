43c0a55b2f45231cbfd7b22f6e42905609f26a3b
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-18 18:08:13 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 112 ms, Memory: 110064 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,28 +1,23 @@
+import math
+from collections import deque
+from itertools import permutations, combinations
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 
-# 양수와 +, -, 괄호를 가지고 식을 만들었음 
-# 괄호를 모두 지웠는데 이후에 이 식의 값을 최소로 만들고 싶음 
-
-# +, - 만 있으므로 -를 기준으로 식을 나누어서 +로 모두 합치고 마지막에 -를 붙이면 됨
-# 55 - 50 + 40 이면 55 - (50 + 40) = -35 가 됨 
-
+# +, -, ()를 가지고 식을 만들었다.
+# 그리고 괄호를 모두 지움 괄호를 쳐서 식의 값을 최소로 만들어야 한다.
+# 10 + 20 - 20 + 10 + 20 - 20 이런식이면 
+# -를 기준으로 뒤에 있는 덧셈을 먼저 하면 최대로 뺄 수 있음 
+# 여기서 첫 요소를 제외한 모든 나머지 항들은 - 항이므로 쉽게 계산 가능 
 
 if __name__ == "__main__":
-    s = input().split('-')
-
-    # print(s)   # ['55 ', ' 50 + 40\n']
-
-    # -로 나눈 식을 +로 나누어서 모두 더하기 
-    first_group = sum(map(int, s[0].split('+'))) # ['10+20+30', '40+50'] 이면 첫 그룹에서 10, 20, 30 을 더함 
-    result = first_group # 첫 그룹의 합을 result에 저장
-
-    for num in s[1:]:
-        result -= sum(map(int, num.split('+')))
+    expression = input().strip().split("-")
+    positive = sum(map(int, expression[0].split("+")))
+    negative = expression[1:]
+    sum_negative = 0
+    for nums in negative:
+        sum_negative += sum(map(int, nums.split("+")))
     
-    print(result)
-
-
+    print(positive - sum_negative)
 
