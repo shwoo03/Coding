5adc5d25d5dc495e048661fdbce156ec8a5d30f2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-23 21:35:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 184 ms, Memory: 17940 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import static java.lang.System.out;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            String inputline = sc.nextLine();
+            out.println(inputline);
+        }
+
+        sc.close();
+    }
+}
+
