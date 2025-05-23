0905c1723a9e484f3861a10d65493d579531c678
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-23 21:42:47 +0900

    [Bronze IV] Title: 세 수, Time: 116 ms, Memory: 110064 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,4 +1,15 @@
-list_num = list(map(int,input().split()))
-list_num.sort()
+import math
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-print(list_num[1])
+
+
+
+
+if __name__ == "__main__":
+    list_num = list(map(int, input().split()))
+    list_num.sort()
+    print(list_num[1])
+    
