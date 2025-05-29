aef46d7f9205c7da0a303fd7f7e9c52133d5483a
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-29 14:19:37 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.py
+++ 백준/Bronze/10171. 고양이/고양이.py
@@ -1,4 +1,14 @@
-print("""\    /\\
- )  ( ')
-(  /  )
- \(__)|""")
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+
+
+if __name__ == "__main__":
+    print("\\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
