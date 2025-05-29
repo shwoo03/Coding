d8e6974c363be97ccd547281b1b6db051f703382
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-05-29 14:24:02 +0900

    [Bronze V] Title: 검증수, Time: 116 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,4 +1,15 @@
-a,b,c,d,e = map(int,input().split())
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-result = a**2 + b**2 + c**2 + d**2 + e**2
-print(result%10)
+
+
+if __name__ == "__main__":
+    list_num = list(map(int, input().split(" ")))
+    for i in range(len(list_num)):
+        list_num[i] = list_num[i] ** 2
+
+    print(sum(list_num) % 10)
