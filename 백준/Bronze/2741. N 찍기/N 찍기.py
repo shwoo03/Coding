8fd3ff01c8b1c0c6af4ecc962558b3f3d7d3bc14
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-01 20:29:00 +0900

    [Bronze V] Title: N 찍기, Time: 128 ms, Memory: 114180 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.py
+++ 백준/Bronze/2741. N 찍기/N 찍기.py
@@ -1,4 +1,13 @@
-n = int(input())
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-for i in range(1,n+1):
-    print(i)
+
+
+if __name__ == "__main__":
+    N = int(input())
+    for i in range(1, N+1):
+        print(i)
