a0a08fb01222be7048a2e4771c7e2c565643fcaf
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-01 20:29:44 +0900

    [Bronze V] Title: 윤년, Time: 112 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,16 @@
-n = int(input())
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-if ((n % 4 == 0) and (n % 100 != 0)) or n % 400 == 0:
-    print("1")
-else:
-    print("0")
+
+
+if __name__ == "__main__":
+    y = int(input())
+
+    if((y % 4 == 0) and (y % 100 != 0)) or (y % 400 == 0):
+        print(1)
+    else:
+        print(0)
