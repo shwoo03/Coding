956e3cc66b59bb4b265bc0fe2ebe814f3e316691
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-02 09:18:05 +0900

    [Bronze V] Title: A+B - 4, Time: 112 ms, Memory: 111280 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -1,6 +1,11 @@
-while True:
-    try:
-        a, b = map(int, input().split())
-        print(a+b)
-    except EOFError:
-        break
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        a, b = map(int, line.split())
+        print(a + b)
