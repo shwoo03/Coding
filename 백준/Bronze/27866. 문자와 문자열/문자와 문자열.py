a1ac72c6842f95fef991347c4b1c451286c9a5de
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-02 09:19:55 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 111284 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.py
@@ -0,0 +1,13 @@
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+
+
+if __name__ == "__main__":
+    word = input().strip()
+    i = int(input())
+    print(word[i-1])
