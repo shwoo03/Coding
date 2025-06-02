f515cc54a8f0ec79d201058488042db818f12b6f
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-02 09:18:02 +0900

    [Bronze V] Title: A+B - 3, Time: 112 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,6 +1,14 @@
-n = int(input())
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-for _ in range(n):
-    a,b = map(int,input().split())
 
-    print(a+b)
+if __name__ == "__main__":
+    T = int(input())
+
+    for i in range(T):
+        a, b = map(int,input().split())
+        print(a+b)
