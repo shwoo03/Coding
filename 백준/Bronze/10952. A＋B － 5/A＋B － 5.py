3abb7e062a9099375f83df73e501c8289054e4c2
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-02 09:18:49 +0900

    [Bronze V] Title: A+B - 5, Time: 112 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.py
@@ -1,5 +1,15 @@
-while True:
-    a,b = map(int,input().split())
-    if a == 0 and b == 0:
-        break
-    print(a+b)
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    while True:
+        a, b = map(int,input().split())
+        if a == 0 and b == 0:
+            break
+        else:
+            print(a+b)
