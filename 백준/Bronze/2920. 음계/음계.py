82319d76348d45ff084f5bf508e64d4d07d0a346
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-03 17:59:23 +0900

    [Bronze II] Title: 음계, Time: 116 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,8 +1,18 @@
-list_num = list(map(int,input().split()))
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-if list_num == sorted(list_num):
-    print("ascending")
-elif list_num == sorted(list_num, reverse=True):
-    print("descending")
-else:
-    print("mixed")
+
+
+if __name__ == "__main__":
+    list_num = list(map(int, input().split()))
+
+    if list_num == sorted(list_num):
+        print("ascending")
+    elif list_num == sorted(list_num, reverse=True):
+        print("descending")
+    else:
+        print("mixed")
