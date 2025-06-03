599bdd57a58ea7ff002d3cf6ef57e4b665b769ec
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-03 12:24:10 +0900

    [Bronze IV] Title: $A + B - C$, Time: 116 ms, Memory: 111284 KB -BaekjoonHub

--- 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -1,6 +1,16 @@
-a = input()
-b = input()
-c = input()
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-print(int(a) + int(b) - int(c))
-print(int(a+b) - int(c))
+
+if __name__ == "__main__":
+    a = input().strip()
+    b = input().strip()
+    c = input()
+
+    ab = a + b
+    print(int(a) + int(b) - int(c))
+    print(int(ab) - int(c))
