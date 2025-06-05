7723309fd1101d83ef60c8a469ebbc7e106d13e0
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-05 13:11:18 +0900

    [Bronze V] Title: 개, Time: 160 ms, Memory: 17032 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import static java.lang.System.out;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        out.println("|\\_/|");  
+        out.println("|q p|   /}");
+        out.println("( 0 )\"\"\"\\"); 
+        out.println("|\"^\"`    |");
+        out.println("||_/=\\\\__|"); 
+
+        sc.close();
+    }
+}
