c3686f516bc4a7b616ae48794061dfab828ff224
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-06 20:54:23 +0900

    [Bronze II] Title: 분해합, Time: 340 ms, Memory: 114316 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,10 +1,23 @@
-n = int(input()) 
-result = 0
-for candidate in range(1, n + 1):
-        decomposition_sum = candidate + sum(map(int, str(candidate)))
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-        if decomposition_sum == n:
-            result = candidate
+
+# 분해합이 들어오면 
+# 그 분해합을 가지는 가장 작은 자연수를 출력하기 
+
+if __name__ == "__main__":
+    N = int(input())
+    result = 0
+
+    for i in range(N+1):
+        decomposition_sum = i + sum(int(num) for num in str(i))
+
+        if decomposition_sum == N:
+            result = i
             break
-
-print(result)
+    
+    print(result)
