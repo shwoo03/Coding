9cc31678ec643217b28341784304113d5c416ce0
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-07 18:47:54 +0900

    [Bronze II] Title: 벌집, Time: 108 ms, Memory: 112476 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,19 +1,27 @@
-# 1 = 1           n = 0
-# 2~7 = 2         n = 1
-# 8~19 = 3        n = 2
-# 20~37 = 4       n = 3
-# 38~61 = 5       n = 4
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
 
-#즉, 처음 6 증가하고 증가값은 6씩 증가된다. 
-# 그렇다면 while 입력값 > 최대 방 번호 를 해서 루프를 돌리면 된다 .
 
-room_num = int(input())
+# 벌집        방 개수 
+# 1 = 1         1
+# 2~7 = 2       6
+# 8~19 = 3      12
+# 20~37 = 4     18
+# 38~61 = 5     24
+#   즉 층 수는 -> 1 + 3 * (층수 - 1) * 층수에 따라서 올라감 
 
-p_num = 1 #벌집 개수 1부터 시작 
-cnt = 1
-while room_num > p_num:
-    p_num += 6 * cnt
-    cnt += 1
+if __name__ == "__main__":
+    N = int(input())
+    floor = 1
+    max_num = 1
 
-print(cnt)
-        
+    while max_num < N:
+        max_num += 6 * floor 
+        floor += 1
+    
+    print(floor)
+
