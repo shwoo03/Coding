bef4fafde8541168879aa77977c5c21758b72332
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-08 17:01:36 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 111284 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,12 +1,28 @@
-l = int(input())
-m = 1234567891
-r = 31
-user_input = input()
- 
-answer = 0
- 
-for i in range(len(user_input)):
-    num = ord(user_input[i]) - 96
-    answer += num * (r ** i)
- 
-print(answer % m)
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+
+# 입력되는 문자는 a~z -> 1~26 으로 매핑한다.
+#   abba는 -> 1,2,2,1 로 나타낼 수 있다.
+#   이러한 과정을 사용해서 충돌이 발생하지 않도록 시그마(a_i * r^i) mod M 을 한다.
+#   r = 31, M = 1234567891
+
+
+
+if __name__ == "__main__":
+    L = int(input())
+    word = input()
+    list_value = []
+
+    r = 31
+    M = 1234567891
+
+    for i in range(L):
+        char_to_int = ord(word[i]) - ord('a') + 1
+        list_value.append(char_to_int * ((pow(r, i)) % M))
+
+    print(sum(list_value))
