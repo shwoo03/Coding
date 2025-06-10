5dcd133d52caa740c697a0115ac65ccfaba60387
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-10 13:23:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import static java.lang.System.out;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String ch = sc.next();
+        out.println((int)ch.charAt(0));
+
+
+        sc.close();
+    }
+}
