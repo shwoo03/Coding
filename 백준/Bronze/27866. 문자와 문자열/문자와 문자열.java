230d5203637d2c1f628939110421623e9d851512
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-12 21:36:12 +0900

    [Bronze V] Title: 문자와 문자열, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import static java.lang.System.out;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+        int n = sc.nextInt();
+        out.println(word.charAt(n-1));
+
+        sc.close();
+    }
+}
