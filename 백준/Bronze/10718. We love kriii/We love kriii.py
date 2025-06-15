7f5105773d747167dae7721fb6a960d078fbc540
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-06-15 16:19:38 +0900

    [Bronze V] Title: We love kriii, Time: 124 ms, Memory: 111272 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.py
+++ 백준/Bronze/10718. We love kriii/We love kriii.py
@@ -1,2 +1,11 @@
-print("""강한친구 대한육군
-강한친구 대한육군""")
+import math
+import heapq
+from collections import deque
+from itertools import permutations, combinations
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    print("강한친구 대한육군")
+    print("강한친구 대한육군")
