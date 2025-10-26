5d1df2a9e1681ef45cd7071b95ff9a8d8b136091
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2025-10-27 02:58:12 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 9768 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+rl.question('', (inputs) => {
+    const [a, b] = inputs.split(' ').map(Number);
+    
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(Math.floor(a / b));
+    console.log(a % b);
+
+    rl.close();
+})
