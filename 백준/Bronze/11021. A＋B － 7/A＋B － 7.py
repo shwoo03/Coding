b4ebe16707b846895761c1a2b220de6565ac4308
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-01-11 17:54:30 +0900

    [Bronze V] Title: A+B - 7, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -1,5 +1,6 @@
-n = int(input())
+loop = int(input())
 
-for i in range(1,n+1):
-    a,b = map(int,input().split())
-    print(f"Case #{i}: {a+b}")
+for _ in range(loop):
+    a, b = map(int, input().split())
+    print(f"Case #{_ + 1}: {a+b}")
+
