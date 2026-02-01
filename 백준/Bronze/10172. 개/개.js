4093193af873bb959fa8eea1f6e922ec18c965bc
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-01 18:27:36 +0900

    [Bronze V] Title: 개, Time: 76 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\s+/);
+
+console.log('|\\_/|');
+console.log('|q p|   /}');
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log('||_/=\\\\__|');
