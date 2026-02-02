132f499425a7cfe3bbc3feecb4782c9d915d5711
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-02 20:50:41 +0900

    [Silver V] Title: 덩치, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,20 +1,18 @@
-# 덩치를 키, 몸무게 두 개의 값으로 등수를 매기려고 한다.
-# a ( x, y ) b ( p, q ) 일 때, x > p, y > q 이면 a가 b보다 덩치가 크다.
-# 하지만 둘 중 한개가 크고 한 개는 작으면 덩치를 비교할 수 없다.
-# 이런 규칙으로 덩치를 비교했을 때 각 사람의 덩치 등수를 출력하라. 덩치를 비교할 수 없으면 공동 순위에 기록한다.
-
 N = int(input())
-
-list_info = []
+list_kg_cm = []
 
 for i in range(N):
-    x, y = map(int, input().split())
-    list_info.append((x, y))
+    temp = list(map(int, input().split()))
+    temp.append(1)
+    list_kg_cm.append(temp)
+    # [[55, 185, 0], ...] 형태 반환 
 
-for i in list_info:
-    rank = 1
-    for j in list_info:
-        if i[0] < j[0] and i[1] < j[1]:
-            rank += 1
-    
-    print(rank, end=' ')
+for i in range(N):
+    for j in range(i+1, N):
+        if (list_kg_cm[i][0] > list_kg_cm[j][0]) and (list_kg_cm[i][1] > list_kg_cm[j][1]):
+            list_kg_cm[j][2] += 1
+        elif (list_kg_cm[j][0] > list_kg_cm[i][0]) and (list_kg_cm[j][1] > list_kg_cm[i][1]):
+            list_kg_cm[i][2] += 1
+
+for i in range(N):
+    print(list_kg_cm[i][2], end=" ")
