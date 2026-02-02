c910136a026aecfdbc562adeac06cda6396238b3
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-03 07:05:56 +0900

    [Silver V] Title: 색종이, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,22 +1,16 @@
-# 가로, 세로 크기를 100인 정사각형 모양의 흰색 도화지가 존재한다.
-# 정사각형 모양의 10 X 10 크기의 색종이를 도화지의 변이 평행하도록 붙인다.
-# 색종이가 붙은 검은 영역의 넓이를 구하는 프로그램을 작성하시오. 
+list_do = [[0 for i in range(100)] for j in range(100)]
 
 N = int(input())
 
-result = 0
-paper = [[0] * 100 for _ in range(100)]
-
-for _ in range(N):
+for a in range(N):
     x, y = map(int, input().split())
-    
-    for i in range(x, x + 10):
-        for j in range(y, y + 10):
-            paper[i][j] = 1
+
+    for x1 in range(x, x + 10):
+        for y1 in range(y, y + 10):
+            list_do[x1][y1] = 1
 
 result = 0
-for row in paper:
+for row in list_do:
     result += row.count(1)
-
 
 print(result)
