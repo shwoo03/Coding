523f94d1c44de737254f48e7bd78d2edde64598b
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-12 16:00:56 +0900

    [Silver IV] Title: 큐, Time: 432 ms, Memory: 113360 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,34 +1,29 @@
-# 정수를 저장하는 큐 구현 
-# push X: 정수 X를 큐에 넣는 연산이다.
-# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-# size: 큐에 들어있는 정수의 개수를 출력한다.
-# empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-# front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-# back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-
-import sys
-
-input = sys.stdin.readline
+from collections import deque
 
 N = int(input())
-queue = []
+dq = deque()
 
 for _ in range(N):
-    cmd = input().split()
-    if cmd[0] == 'push':
-        queue.append(cmd[1])
-    elif cmd[0] == 'pop':
-        if queue:
-            print(queue.pop(0))
+    command = input().split()
+    if command[0] == "push":
+        value = int(command[1])
+        dq.append(value)
+    elif command[0] == "pop":
+        if dq:
+            print(dq.popleft())
         else:
             print(-1)
-    elif cmd[0] == 'size':
-        print(len(queue))
-    elif cmd[0] == 'empty':
-        print(0 if queue else 1)
-    elif cmd[0] == 'front':
-        print(queue[0] if queue else -1)
-    elif cmd[0] == 'back':
-        print(queue[-1] if queue else -1)
-    else:
-        pass
+    elif command[0] == "size":
+        print(len(dq))
+    elif command[0] == "empty":
+        print(0 if dq else 1)
+    elif command[0] == "front":
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif command[0] == "back":
+        if dq:
+            print(dq[-1])
+        else:
+            print(-1)
