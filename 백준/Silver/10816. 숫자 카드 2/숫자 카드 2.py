b9cd82a4c785bec477f9932a1e0e424f28777a7a
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-14 17:07:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 420 ms, Memory: 260500 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,21 +1,19 @@
-# 정수가 하나 적혀있는 카드 N 개를 가지고 있다.
-# 정수 M개가 주어졌을 때, 이 수가 적혀있는 카드를 몇개 가지고 있는지 구하는 프로그램 작성 
-
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-cards = list(map(int, input().split()))
+list_num = list(map(int, input().split()))
 
 M = int(input())
-find = list(map(int, input().split()))
+dict_num = {
+    
+}
 
-count_dict = {}
-for card in cards:
-    if card in count_dict:
-        count_dict[card] += 1
-    else:
-        count_dict[card] = 1
+list_input = list(map(int, input().split()))
 
-for num in find:
-    print(count_dict.get(num, 0), end=' ')
+for i in list_input:
+    dict_num[i] = 0
+
+for i in list_num:
+    if i in dict_num:
+        dict_num[i] += 1
+
+for i in list_input:
+    print(dict_num[i], end=' ')
