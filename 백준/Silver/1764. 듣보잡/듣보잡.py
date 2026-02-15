72f9f244a180aafb3137313d5775bc557bcc9b25
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-15 13:25:28 +0900

    [Silver IV] Title: 듣보잡, Time: 152 ms, Memory: 120664 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,18 +1,17 @@
-# 모르는 사람의 명단이 주어질 때, 명단을 구하는 프로그램 작성
-# N은 듣지 못한 사람의 수, M은 보지 못한 사람의 수
-
 N, M = map(int, input().split())
-list_N = []
-list_M = []
 
+set_N = set()
 for i in range(N):
-    list_N.append(input())
+    set_N.add(input())
 
+set_M = set()
 for i in range(M):
-    list_M.append(input())
+    set_M.add(input())
 
-result = list(set(list_N) & set(list_M))
-result.sort()
-print(len(result))
-for i in result:
+# 교집합 구하기
+answer_set = set_N & set_M
+answer_list = sorted(answer_set)
+
+print(len(answer_list))
+for i in answer_list:
     print(i)
