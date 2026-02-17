90fb39688b3c1ebe431404bed1e0ee185dd85acc
woo seung hoon <99020376+shwoo03@users.noreply.github.com>
2026-02-17 14:38:13 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 148 ms, Memory: 111944 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,44 +1,45 @@
-# 어떤 문자열이 주어지면, 관호들의 균형이 잘 맞춰져 있는지 확인하는 프로그램 작성 
-# 모든 왼쪽 소괄호는 오른쪽 소괄호와만 짝을 이룬다.
-# 모든 왼쪽 대괄호는 오른쪽 대괄호와만 짝을 이룬다.
-# 모든 오른쪽 괄호들은 자신과 짝을 이룰 수 있는 왼쪽 괄호가 존재한다.
-# 모든 괄호들의 짝은 1:1 매칭만 가능하다. 즉, 괄호 하나가 둘 이상의 괄호와 짝지어지지 않는다.
-# 짝을 이루는 두 괄호가 있을 때, 그 사이에 있는 문자열도 균형이 잡혀야 한다. 
-# " ." 와 같이 괄호가 하나도 없는 경우도 균형잡힌 문자열로 간주할 수 있다. 
-
-# 각 줄마다 해당 문자열이 균형을 이루면 yes 아니면 no 출력 
-
-import sys
-
-input = sys.stdin.readline
-list_s = []
+strings = []
+stack = []
 
 while True:
-    s = input().rstrip()
-    if s == '.':
-        break
-    list_s.append(s)
+    temp = input()
 
-for s in list_s:
-    stack = []
-    check = True
-    
-    for i in s:
-        if i == '(' or i == '[':
-            stack.append(i)
-        elif i == ')':
-            if not stack or stack[-1] == '[':
-                check = False
-                break
-            elif stack[-1] == '(':
-                stack.pop()
-        elif i == ']':
-            if not stack or stack[-1] == '(':
-                check = False
-                break
-            elif stack[-1] == '[':
-                stack.pop()
-    if check and not stack:
-        print('yes')
+    if temp == ".":
+        break
     else:
-        print('no')
+        strings.append(temp)
+
+for string in strings:
+    # 아 코드 이상한데 ;;;; 그냥 할래 
+    stack = []
+    for char in string:
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            if len(stack) == 0:
+                print("no")
+                break
+            else:
+                if stack[-1] == "(":
+                    stack.pop()
+                else:
+                    print("no")
+                    break
+        elif char == "[":
+            stack.append(char)
+        elif char == "]":
+            if len(stack) == 0:
+                print("no")
+                break
+            else:
+                if stack[-1] == "[":
+                    stack.pop()
+                else:
+                    print("no")
+                    break
+    else:
+        if len(stack) == 0:
+            print("yes")
+        else:
+            print("no")
+
